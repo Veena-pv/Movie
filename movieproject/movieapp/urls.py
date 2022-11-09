53fe02f549e11fc75a060212db484e3a2a4be483
Veena-pv <veenapv99@gmail.com>
2022-11-09 15:08:12 +0530

    added

---
+++ movieproject/movieapp/urls.py
@@ -0,0 +1,16 @@
+
+from django.urls import path
+
+from movieproject import settings
+from . import views
+from django.conf.urls.static import static
+app_name='movieapp'
+urlpatterns = [
+
+    path('', views.index, name='index'),
+    path('movie/<int:movie_id>/', views.detail, name='detail'),
+    path('add/', views.add_movie, name='add'),
+    path('update/<int:id>/', views.update, name='update'),
+    path('delete/<int:id>/', views.delete, name='delete')
+]
+
